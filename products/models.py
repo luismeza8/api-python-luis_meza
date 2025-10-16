ee185945f6a4c82cb0b6cd638b1dc48df9917b70
luismeza8 <angel841bv@gmail.com>
2025-10-16 11:09:07 -0700

    feat: add Producto and Usuario models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255)
+    precio = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(0)]
+    )
+    stock = models.PositiveIntegerField()
+    activo = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+
+
+    def alternar_activo(self, commit=True):
+        self.activo = not self.activo
+        
+        if commit:
+            self.save(update_fields=['activo', 'last_update'])
+        
+        return self.activo
+
+    
+    def __str__(self):
+        return self.nombre
--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    edad = models.PositiveIntegerField()
+    activo = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+
+
+    def alternar_activo(self, commit=True):
+        self.activo = not self.activo
+        
+        if commit:
+            self.save(update_fields=['activo', 'last_update'])
+        
+        return self.activo
+
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
