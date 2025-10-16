e762256c20bd64949954d65b629b69087475a7e9
luismeza8 <angel841bv@gmail.com>
2025-10-16 09:54:31 -0700

    chore: connect postgresql

--- api_python/settings.py
+++ api_python/settings.py
@@ -1,3 +1,8 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
 """
 Django settings for api_python project.
 
@@ -76,8 +81,12 @@ WSGI_APPLICATION = 'api_python.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
