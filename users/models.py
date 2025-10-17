0749801d773b75c3d3b1451776867f04be92c300
luismeza8 <angel841bv@gmail.com>
2025-10-17 07:44:54 -0700

    fix: delete unnecessary method on Usuario model

--- users/models.py
+++ users/models.py
@@ -10,14 +10,5 @@ class Usuario(models.Model):
     last_update = models.DateTimeField(auto_now=True)
 
 
-    def alternar_activo(self, commit=True):
-        self.activo = not self.activo
-        
-        if commit:
-            self.save(update_fields=['activo', 'last_update'])
-        
-        return self.activo
-
-
     def __str__(self):
         return f'{self.nombre} {self.apellido}'
